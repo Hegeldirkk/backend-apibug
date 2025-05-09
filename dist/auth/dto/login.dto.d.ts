35e70446d8386e0a77b4bffb69faabe5fced7a27
Diabakate Ikary <hegeldirkk@gmail.com>
2025-05-09 11:19:17 +0000

    update da

--- dist/auth/dto/login.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { UserRole } from 'src/user/user.entity';
-export declare class LoginDto {
-    email: string;
-    password: string;
-    role: UserRole;
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -23,14 +23,26 @@ import { ReportMessageModule } from './report-message/report-message.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
+      host: process.env.DATABASE_HOST || '127.0.0.1',
+      port: Number(process.env.DATABASE_PORT) || 3306,
+      username: process.env.DATABASE_USER || 'root',
+      password: process.env.DATABASE_PASSWORD || '',
+      database: process.env.DATABASE_NAME || 'bounty',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Désactiver en production
+      //connectTimeout: 30000, // Timeout augmenté à 30 sec
+      
+    }),
+    /*TypeOrmModule.forRoot({
+      type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
       password: '',
       database: 'bugproject',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [__dirname + '//*.entity{.ts,.js}'],
       synchronize: true, // ❗ À désactiver en prod
-    }),
+    }),*/
     AuthModule,
     ProgramModule,
     ReportModule,
